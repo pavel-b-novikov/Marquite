e9c8aa1d9f822937edd1a7a6e6643ee163e258ce
Pavel Novikov <pavel.b.novikov@gmail.com>
2015-05-11 02:44:01 +0600

    MVC compatibility

---
+++ Marquite.Core/Html/DisplayNameExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Marquite.Core.Html
+{
+    public static class DisplayNameExtensions
+    {
+        public static MvcHtmlString DisplayName(this HtmlHelper html, string expression)
+        {
+            return System.Web.Mvc.Html.DisplayNameExtensions.DisplayName(html, expression);
+        }
+
+        
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is an appropriate nesting of generic types")]
+        public static MvcHtmlString DisplayNameFor<TModel, TValue>(this HtmlHelper<IEnumerable<TModel>> html, Expression<Func<TModel, TValue>> expression)
+        {
+            return System.Web.Mvc.Html.DisplayNameExtensions.DisplayNameFor(html, expression);
+        }
+
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", Justification = "This is an extension method")]
+        
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is an appropriate nesting of generic types")]
+        public static MvcHtmlString DisplayNameFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            return System.Web.Mvc.Html.DisplayNameExtensions.DisplayNameFor(html, expression);
+        }
+
+        public static MvcHtmlString DisplayNameForModel(this HtmlHelper html)
+        {
+            return System.Web.Mvc.Html.DisplayNameExtensions.DisplayNameForModel(html);
+        }
+        
+    }
+}
---
+++ Marquite.Core/Html/DisplayTextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Marquite.Core.Html
+{
+    public static class DisplayTextExtensions
+    {
+        public static MvcHtmlString DisplayText(this HtmlHelper html, string name)
+        {
+            return System.Web.Mvc.Html.DisplayTextExtensions.DisplayText(html, name);
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is an appropriate nesting of generic types")]
+        public static MvcHtmlString DisplayTextFor<TModel, TResult>(this HtmlHelper<TModel> html, Expression<Func<TModel, TResult>> expression)
+        {
+            return System.Web.Mvc.Html.DisplayTextExtensions.DisplayTextFor(html, expression);
+        }
+    }
+}
